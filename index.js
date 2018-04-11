bc1c4b6975daa3a4516abd7c3a2380c66ff4c605
Oleksandr Breslavets <oleksandr.breslavets@stryker.com>
2018-04-11 16:53:35 +0300

    Save on disk added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var robotjs = require('./build/Release/robotjs.node');
+var Jimp = require('jimp');
 
 module.exports = robotjs;
 
@@ -34,3 +35,25 @@ module.exports.screen.capture = function(x, y, width, height)
 
     return new bitmap(b.width, b.height, b.byteWidth, b.bitsPerPixel, b.bytesPerPixel, b.image);
 };
+
+module.exports.screen.save = function (robotScreenPic, path) {
+    return new Promise((resolve, reject) => {
+        try {
+            const image = new Jimp(robotScreenPic.width, robotScreenPic.height);
+            let pos = 0;
+            image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
+                /* eslint-disable no-plusplus */
+                image.bitmap.data[idx + 2] = robotScreenPic.image.readUInt8(pos++);
+                image.bitmap.data[idx + 1] = robotScreenPic.image.readUInt8(pos++);
+                image.bitmap.data[idx + 0] = robotScreenPic.image.readUInt8(pos++);
+                image.bitmap.data[idx + 3] = robotScreenPic.image.readUInt8(pos++);
+                /* eslint-enable no-plusplus */
+            });
+            image.write(path)
+            resolve();
+        } catch (e) {
+            console.error(e);
+            reject(e);
+        }
+    });
+}
