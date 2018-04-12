e0b3f32dfda15cf51b5a629992bfa36b5b8f829b
Oleksandr Breslavets <oleksandr.breslavets@stryker.com>
2018-04-12 11:52:02 +0300

    clean up

--- test/screen.js
+++ test/screen.js
@@ -2,7 +2,7 @@ var robot = require('..');
 var pixelColor, screenSize;
 
 describe('Screen', () => {
-  xit('Get pixel color.', function()
+  it('Get pixel color.', function()
   {
     expect(pixelColor = robot.getPixelColor(5, 5)).toBeTruthy();
     expect(pixelColor !== undefined).toBeTruthy();
@@ -30,14 +30,10 @@ describe('Screen', () => {
     }).toThrowError(/Invalid number/);
   });
 
-  xit('Get screen size.', function()
+  it('Get screen size.', function()
   {
     expect(screenSize = robot.getScreenSize()).toBeTruthy();
     expect(screenSize.width !== undefined).toBeTruthy();
     expect(screenSize.height !== undefined).toBeTruthy();
-  });
-
-  it('My Test', function () {
-    robot.screen.captureToFile('d:\\local.bmp');
   });
 });
